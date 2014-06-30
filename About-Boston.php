a8bfcc9e420889bc3798c6a2825a011662f3e200
LakshmiManjunath <lakshmi.cs87@gmail.com>
2014-06-30 10:46:26 -0400

    changed file name from .html to .php

---
+++ About-Boston.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>About Boston</title>
+</head>
+<body>
+<h1>Boston is a vibrant and energetic city!!</h1>
+<img src="image/boston.jpg" alt="boston">
+</body>
+</html>
